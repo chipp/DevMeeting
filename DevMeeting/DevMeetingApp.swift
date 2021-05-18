4fa2fa804d0b4e6db7085a89fe836d03a2171084
Vladimir Burdukov <chippcheg@gmail.com>
2021-05-18 16:21:56 +0300

    Initial Commit

---
+++ DevMeeting/DevMeetingApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2021-05-17
+//
+
+import SwiftUI
+
+@main
+struct DevMeetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
